9d7064152dcaecc62cc9319098e1bc4080330d9c
kailu90 <czuluaga2018@gmail.com>
2024-04-24 07:55:40 -0500

    Se organiza función de flechas del slider

--- JavaScript.js
+++ JavaScript.js
@@ -1,23 +1,10 @@
-//-----------------------Función animación SLIDER--------------------//
-
 let slider = document.querySelector(".slider_container_elements");
 
-let index = 1;
+let index = 0;
 
 let numImages = 4;
 
-setInterval(function () {
-    let percentage = index * -100;
-    slider.style.transition = "transform 1s ease";
-    slider.style.transform = "translateX(" + percentage + "%)";
-    index++;   
-    if (index >= numImages) {
-        index = 0; 
-    }  
-},10000);
-
-
-
+let percentage = 0;
 
 
 //-----------------------Función flecha ATRÁS SLIDER--------------------//
@@ -27,20 +14,21 @@ let arrowBack = document.querySelector(".arrow_back")
 let arrowNext = document.querySelector(".arrow_next")
 
 arrowBack.addEventListener("click" , function() {
-    console.log("click flecha atrás");
-    let percentage = index * 100;
-    slider.style.transition = "transform 1s ease";
-    slider.style.transform = "translateX(" + percentage + "%)";
-    index++;
-    if (index = 0) {
-        index = 1;
-        let percentage = index * 300;
+    if (percentage === 0) {
+        console.log("prueba if");
+        percentage -= 300;    
         slider.style.transition = "transform 1s ease";
-        slider.style.transform = "translateX(" + percentage + "%)";  
-    }  
+        slider.style.transform = "translateX(" + percentage + "%)";
+        console.log(percentage);  
+    }
+    else {
+        console.log("click flecha atrás");
+        percentage += 100;    
+        slider.style.transition = "transform 1s ease";
+        slider.style.transform = "translateX(" + percentage + "%)";
+        console.log(percentage);   
+    }
 });
-
-
 
 
 
@@ -48,18 +36,39 @@ arrowBack.addEventListener("click" , function() {
 
 
 arrowNext.addEventListener("click" , function() {
-    console.log("click flecha siguiente");
-    let percentage = index * 100;
+    if (percentage === -300) {
+        console.log("prueba if next");
+        percentage += 300;    
+        slider.style.transition = "transform 1s ease";
+        slider.style.transform = "translateX(" + percentage + "%)";
+        console.log(percentage);  
+    }
+
+    else {
+        console.log("click flecha siguiente");
+        percentage -= 100;    
+        slider.style.transition = "transform 1s ease";
+        slider.style.transform = "translateX(" + percentage + "%)";
+        console.log(percentage);   
+    } 
+    
+});
+
+//-----------------------Función animación SLIDER--------------------//
+
+setInterval(function () {   
+    let percentage = index * -100;
     slider.style.transition = "transform 1s ease";
     slider.style.transform = "translateX(" + percentage + "%)";
-    index++;
-    if (index = 0) {
-        index = 1;
-        let percentage = index * 300;
-        slider.style.transition = "transform 1s ease";
-        slider.style.transform = "translateX(" + percentage + "%)";  
+    index++; 
+    if (index >= numImages) {
+        index = 0; 
     }  
-});
+},10000);
+
+
+
+
 
 
 //-----------------------Función botón NAVEGACION SLIDER--------------------//
